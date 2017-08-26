95aec84749a007c4aa301bd3ea9b5740ea70ba8f
Liam Barstad <liambarstad@gmail.com>
2017-08-26 15:03:33 -0600

    Add article model, tests(with shoulda-matchers), and migration

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe "validations" do
+
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+    it { should validate_presence_of(:body) }
+
+  end
+
+  describe "relationships" do
+
+  end
+end
